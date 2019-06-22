d872aec941dd16f82368d300f7471a46de69a855
Mickael Moreira <dopidot@gmail.com>
2019-06-22 23:21:45 +0200

    Finish with signUp page and add creation of wishlist (in progress)

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { Wishlist, WishlistApi, User } from '../../shared/sdk';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+    selector: 'app-create',
+    templateUrl: './create.component.html',
+    styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
+    success_message = null;
+    error_message = null;
 
-  ngOnInit() {
-  }
+    constructor(private wishlistApi: WishlistApi, private formBuilder: FormBuilder, private router: Router) { }
+
+    ngOnInit() {
+        this.createForm();
+    }
+
+    private createForm() {
+        this.form = this.formBuilder.group({
+            name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
+            public: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(30)]]
+        });
+    }
+
+    onSubmit() {
+
+        this.error_message = null;
+        this.success_message = null;
+
+        const name = this.form.controls.name.value;
+        const isPublic = this.form.controls.public.value;
+
+        let isValid = this.checkValidators();
+
+        if (!isValid)
+            return;
+
+        let wishlist = new Wishlist();
+        wishlist.name = name;
+        wishlist.isPublic = isPublic;
+
+        let user = new User();
+        user.id = parseInt(localStorage.getItem("id"), 10);
+        wishlist.owner = user; 
+
+        this.wishlistApi.create(wishlist).subscribe((item) => {
+            console.log(item);
+            this.success_message = "The wishlist has been successfully created !";
+        }, error => {
+            console.log(error);
+            this.error_message = "An error has occurred. Please check your information and try again.";
+        });
+    }
+
+    checkValidators() {
+        if (this.form.controls.name.value.length < 4 || this.form.controls.name.value.length > 30) {
+            this.error_message = "The name must be between 4 and 30 characters long.";
+            return false;
+        }
+
+        return true;
+    }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../shared/sdk';
-import { BaseSailsApi } from '../../shared/sdk/services/core/base.service';
+import { User, UserApi } from '../../shared/sdk';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
 
@@ -15,7 +14,7 @@ export class SignupComponent implements OnInit {
     success_message = null;
     error_message = null;
 
-    constructor(private baseApi: BaseSailsApi, private formBuilder: FormBuilder, private router: Router) { }
+    constructor(private userApi: UserApi, private formBuilder: FormBuilder, private router: Router) { }
 
     ngOnInit() {
         this.createForm();
@@ -23,9 +22,9 @@ export class SignupComponent implements OnInit {
 
     private createForm() {
         this.signUpForm = this.formBuilder.group({
-            firstName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-            lastName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-            email: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(50)]],
+            firstName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
+            lastName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
+            email: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]],
             password1: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(20)]],
             password2: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(20)]]
         });
@@ -33,11 +32,19 @@ export class SignupComponent implements OnInit {
 
     onSubmit() {
 
+        this.error_message = null;
+        this.success_message = null;
+
         const firstName = this.signUpForm.controls.firstName.value;
         const lastName = this.signUpForm.controls.lastName.value;
         const email = this.signUpForm.controls.email.value;
         const password1 = this.signUpForm.controls.password1.value;
         const password2 = this.signUpForm.controls.password2.value;
+
+        let isValid = this.checkValidators();
+
+        if(!isValid)
+            return;
 
         if(password1 != password2)
         {
@@ -51,13 +58,48 @@ export class SignupComponent implements OnInit {
         user.email = email;
         user.password = password1;
 
-        this.baseApi.create<User>(user).subscribe((item) => {
-            console.log(item);
+        this.userApi.create(user).subscribe((item) => {
+            //console.log(item);
             this.success_message = "The user has been successfully created !";
         }, error => {
-            console.log(error);
-            this.error_message = "An error has occured.";
+            //console.log(error);
+            this.error_message = "An error has occurred. Please check your information and try again.";
         });
+    }
+
+    checkValidators()
+    {
+        if(this.signUpForm.controls.firstName.value.length < 4 || this.signUpForm.controls.firstName.value.length > 30)
+        {
+            this.error_message = "The firstname must be between 4 and 30 characters long.";
+            return false;
+        }
+
+        if(this.signUpForm.controls.lastName.value.length < 4 || this.signUpForm.controls.lastName.value.length > 30)
+        {
+            this.error_message = "The lastname must be between 4 and 30 characters long.";
+            return false;
+        }
+
+        if(this.signUpForm.controls.email.value.length < 8 || this.signUpForm.controls.email.value.length > 30)
+        {
+            this.error_message = "The email must be between 8 and 30 characters long.";
+            return false;
+        }
+
+        if(this.signUpForm.controls.password1.value.length < 6 || this.signUpForm.controls.password1.value.length > 20)
+        {
+            this.error_message = "The password must be between 6 and 20 characters long.";
+            return false;
+        }
+
+        if(this.signUpForm.controls.password2.value.length < 6 || this.signUpForm.controls.password2.value.length > 20)
+        {
+            this.error_message = "The confirmation password must be between 6 and 20 characters long.";
+            return false;
+        }
+
+        return true;
     }
 
 }
