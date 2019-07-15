933b54e0b096201d2064a96cd579531d65c55d1e
Mickael Moreira <dopidot@gmail.com>
2019-07-15 23:53:17 +0200

    Init Chrome extension with Amazon

---
+++ Amazon Extension/content.js
@@ -0,0 +1,32 @@
+// content.js
+alert("Hello from your Chrome extension!")
+
+// content.js
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+var wishlistUrl = 'http://localhost:4200/';
+
+console.log(firstHref);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
+
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+
+      // This line is new!
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": wishlistUrl});
+    }
+  }
+);
